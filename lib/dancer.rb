dfd65c3ca2fc52202832a8c3ff294df198b54fff
Elijah wines <elijahwines84@yahoo.com>
2020-03-10 21:04:06 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dacncer 
+  include Dance 
+
+  attr_accessor :name 
+
+  def initalize(name)
+    @name = name 
+  end 
+  
+end
